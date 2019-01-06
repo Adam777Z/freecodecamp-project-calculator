892baa7e292368ccfd5b953e2dc543b5874cb54f
Adam777 <Adam777-5867@outlook.com>
2019-01-06 11:12:26 +0000

    Update

--- js/index.js
+++ js/index.js
@@ -30,7 +30,7 @@ const keys = {
 	111: 'divide',
 	13: 'equals',
 	46: 'clear'
-}
+};
 
 var input = 0;
 var operation = '';
@@ -60,10 +60,10 @@ function handleOperator(operatorThis) {
 	} else {
 		operation += input + operatorThis;
 	}
-	
+
 	operator = operatorThis;
 	input = 0;
-	
+
 	$('#operation').html(operation.length > 14 ? '...' + operation.substring(operation.length - 14) : operation);
 	$('#display').html(input);
 }
@@ -82,9 +82,9 @@ $(document).ready(function() {
 			operation += input;
 			operator = '=';
 		}
-		
+
 		input = eval(operation);
-		
+
 		$('#operation').html(operation.length > 14 ? '...' + operation.substring(operation.length - 14) : operation);
 		$('#display').html(input.toString().length > 14 ? Number.parseFloat(input).toPrecision(10).replace(/0+$/, '') : input);
 	});
@@ -95,59 +95,59 @@ $(document).ready(function() {
 			$('#display').html(input);
 		}
 	});
-	
+
 	$('#add').click(function() {
 		handleOperator('+');
 	});
-	
+
 	$('#subtract').click(function() {
 		handleOperator('-');
 	});
-	
+
 	$('#multiply').click(function() {
 		handleOperator('*');
 	});
-	
+
 	$('#divide').click(function() {
 		handleOperator('/');
 	});
-	
+
 	$('#zero').click(function() {
 		handleNumber(0);
 	});
-	
+
 	$('#one').click(function() {
 		handleNumber(1);
 	});
-	
+
 	$('#two').click(function() {
 		handleNumber(2);
 	});
-	
+
 	$('#three').click(function() {
 		handleNumber(3);
 	});
-	
+
 	$('#four').click(function() {
 		handleNumber(4);
 	});
-	
+
 	$('#five').click(function() {
 		handleNumber(5);
 	});
-	
+
 	$('#six').click(function() {
 		handleNumber(6);
 	});
-	
+
 	$('#seven').click(function() {
 		handleNumber(7);
 	});
-	
+
 	$('#eight').click(function() {
 		handleNumber(8);
 	});
-	
+
 	$('#nine').click(function() {
 		handleNumber(9);
 	});
